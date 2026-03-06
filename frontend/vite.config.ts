3b49e98ad0c526805cd91d25563cc88a00e7752b
ChillWoW <97155839+ChillWoW@users.noreply.github.com>
2026-03-06 21:52:48 +0200

    feat: setup tanstack router

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,11 +1,12 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), tailwindcss()],
+    plugins: [TanStackRouterVite(), react(), tailwindcss()],
     base: "./",
     build: {
         outDir: "build"
@@ -14,6 +15,7 @@ export default defineConfig({
         port: 3500
     },
     resolve: {
+        dedupe: ["react", "react-dom"],
         alias: {
             "@": path.resolve(__dirname, "./src")
         }
